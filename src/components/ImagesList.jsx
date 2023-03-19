bbbd22890c5a9f68d329ba4336735be02a91214c
CeliaGC <you@example.comgarciacastillacelia@gmail.com>
2023-03-19 23:53:36 +0100

    styled-navbar

--- src/components/ImagesList.jsx
+++ src/components/ImagesList.jsx
@@ -29,23 +29,30 @@ function ImagesList() {
   return (
 
     
-       <Row style={{gap: 1, justifyContent:'space-evenly', paddingTop:'30px'}} xs={1} md={2} lg={5} className="g-4">
-            <Col style={{justifyContent:'space-evenly'}} xs={4} lg={4}> </Col>    
+    <Row style={{gap: 0.5, flexDirection:"row-reverse" ,justifyContent:'space-evenly', paddingTop:'30px'}} xs={1} md={2} lg={4} className="g-4"> 
+
+    <Col style={{justifyContent:'space-evenly'}} xs={4} lg={3}> </Col>   
+    <Col style={{justifyContent:'space-evenly'}} xs={4} lg={3}> </Col>
+    <Col style={{justifyContent:'space-evenly'}} xs={4} lg={3}> </Col>
+    <Col style={{justifyContent:'space-evenly'}} xs={4} lg={3}> </Col> 
 
                 {
                   myImages.map(i => (
                   
                   
-                    <Card key={i.id} style={{backgroundColor:"beige"}}>
+                    <Card key={i.id} style={{backgroundColor:"rgba(135, 135, 135, 0.7)"}}>
                     
-                    <Card.Img variant="bottom" src={i.imageSource} />
+                    <Card.Img variant="top" src={i.imageSource} style={{marginTop: "0.3125rem", }} />
+                    <Card.ImgOverlay style={{margin: "-10px", height:"1.875rem"}}>
                     <Card.Body>
-                      <Card.Title>{i.imageName}</Card.Title>
-                      <Card.Text>
-                      </Card.Text>
-                      <Link to={`/editImage/${i.id}`} ><Button variant="primary">Edit</Button></Link>
-                      <Button variant="primary" onClick={()=>deleteImage(i.id)}>Delete</Button>
-                    </Card.Body>
+                      <Card.Title style={{backgroundColor:"rgba(233,236,239,0.6"}}>{i.imageName} </Card.Title>
+
+                      </Card.Body>
+                      </Card.ImgOverlay>
+                      <Card.Footer style={{display:"flex", flexDirection:"row",justifyContent:'space-evenly', alignContent:"flex-end"}}>
+                      <Link to={`/editImage/${i.id}`} ><Button variant="outline-light" size='sm'>Edit</Button></Link>
+                      <Button variant="outline-light" size='sm' onClick={()=>deleteImage(i.id)}>Delete</Button>
+                      </Card.Footer>
                     </Card>
 
                   
@@ -54,7 +61,7 @@ function ImagesList() {
                 }
           
                  
-                  {/* ))} */}
+              
                  </Row> 
   );
 }
