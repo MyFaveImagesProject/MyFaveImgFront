93ee2d7ce0cff5b6fdaf1ddcc89107a757645a44
CeliaGC <you@example.comgarciacastillacelia@gmail.com>
2023-03-21 01:47:08 +0100

    uploadForm-ready-steady

--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -1,14 +1,10 @@
 import { useForm } from 'react-hook-form';
 import React, { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
 import ImageHandler from '../handler/ImageHandler';
 import Button from 'react-bootstrap/Button';
-
 import Form from 'react-bootstrap/Form';
-import Alert from 'react-bootstrap/Alert';
-
 import { Link } from 'react-router-dom';
-
-
 
 
 
@@ -16,7 +12,7 @@ function UpLoadForm() {
   const { register, handleSubmit, formState: { errors }, setValue, getValues } = useForm();
 
   const handleImageChange = (event) => {
-    const picture = event.target.files[0];
+    const picture = event.target.input;
     const reader = new FileReader();
     reader.readAsDataURL(picture);
     reader.onload = () => {
@@ -36,20 +32,13 @@ function UpLoadForm() {
 
   const onSubmit = (data) => {
     ImageHandler.addImage(data)
-    console.log(data)
-    // return window.alert("Producto agregado a su cuenta");
+
 }
-// const newId = 0; 
-// const newGuid = uuidv4();
-// const date = new Date();
-// const isActive = 1
 
   return (
 
     <>
-    
-    
-      <Form style={{borderRadius:"0.625rem", border:"2px solid #d63384", height:"80%", width:"80%", marginTop:"10%", marginLeft:"10%", backgroundColor:"rgba(153, 153, 153, 0.7)"}} onSubmit={handleSubmit(onSubmit)}>
+     <Form style={{borderRadius:"0.625rem", border:"2px solid #d63384", height:"80%", width:"80%", marginTop:"10%", marginLeft:"10%", backgroundColor:"rgba(153, 153, 153, 0.7)"}} onSubmit={handleSubmit(onSubmit)}>
       <h2 style={{backgroundColor:"rgba(153, 153, 153, 1)", borderRadius:"0.625rem", border:"2px solid #d63384"}}>New Image</h2>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label htmlFor="id"></Form.Label>
@@ -58,13 +47,13 @@ function UpLoadForm() {
     
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label htmlFor="imageName">Title</Form.Label>
-            <Form.Control name='imageName' style={{backgroundColor:"rgba(255, 233, 246, 1)",marginLeft:"10%", width:"80%"}} {...register("terms", { required: true })} />
+            <Form.Control name='imageName' style={{backgroundColor:"rgba(255, 233, 246, 1)",marginLeft:"10%", width:"80%"}} {...register("imageName", { required: true })} />
          {errors.terms && <span>Debe rellenar este campo</span>}
           </Form.Group>
     
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label htmlFor="category">Category</Form.Label>
-                        <select style={{borderRadius:"0.0625rem", backgroundColor:"rgba(255, 233, 246, 1)"}}{...register("terms", { required: true })} name="category" >
+                        <select style={{borderRadius:"0.0625rem", backgroundColor:"rgba(255, 233, 246, 1)"}}{...register("category", { required: true })} name="category" >
                           <option value="nature">Nature</option>
                           <option value="society">Society</option>
                           <option value="science">Science</option>
@@ -73,8 +62,8 @@ function UpLoadForm() {
           </Form.Group>
     
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label htmlFor="imageSource">Image Source</Form.Label>
-            <Form.Control style={{backgroundColor:"rgba(255, 233, 246, 1)", width:"80%",marginLeft:"10%"}} {...register("terms", { required: true })} name="imageSource" />
+            <Form.Label htmlFor="ImageSource">Image Source</Form.Label>
+            <Form.Control style={{backgroundColor:"rgba(255, 233, 246, 1)", width:"80%",marginLeft:"10%"}} {...register("ImageSource", { required: true })} name="ImageSource" placeholder='URL' onChange={handleImageChange}/>
             {errors.terms && <span>Debe rellenar este campo</span>}
           </Form.Group>
     
@@ -102,10 +91,13 @@ function UpLoadForm() {
                      </Alert>
                 </Form>
                 
-          </>
+          </> 
 
- )
+     
+  )
 
 }
 
 export default UpLoadForm
+    
+      
