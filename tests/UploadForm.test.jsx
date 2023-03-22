d37e2bd316c4bc216d4bca10a7d0111c73354335
CeliaGC <you@example.comgarciacastillacelia@gmail.com>
2023-03-22 15:33:40 +0100

    app-ready

--- tests/UploadForm.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom'
-import UpLoadForm from "../src/components/UploadForm";
-
-test('should render app title', () => { 
-    render(<UpLoadForm />); 
-    const titleElement = screen.getAllByPlaceholderText(/URL/i); 
-    expect(titleElement).toBeInTheDocument(); 
-});
